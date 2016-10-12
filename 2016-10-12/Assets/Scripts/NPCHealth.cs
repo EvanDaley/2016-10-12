e1006be6cfbb11f7c30608fd04e03464a7b4cd7c
Evan <daley.evan@gmail.com>
2016-10-12 12:58:39 -0700

    Absorb yesterdays work into today's project
    I'm trying to make drastic improvements to this game every day. Today
    I'm going to add story elements with triggered events.

---
+++ 2016-10-12/Assets/Scripts/NPCHealth.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class NPCHealth : MonoBehaviour {
+
+	public Text text;
+	public float health = 100f;
+	public bool alive = true;
+	public Renderer rend;
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (health < 1 && alive)
+		{
+			alive = false;
+			Die ();
+		}
+	}
+
+	void OnCollisionEnter()
+	{
+		health -= 10;
+	}
+
+	void Die()
+	{
+		if (text != null)
+		{
+			text.text = "D'oh!";
+			text.gameObject.transform.parent.gameObject.AddComponent<DieAfterTime> ();
+		}
+
+		if(rend!= null)
+			rend.enabled = false;
+
+		NPCCombat combat = gameObject.GetComponent<NPCCombat> ();
+		if (combat != null)
+			combat.enabled = false;
+
+		Rigidbody rbody = gameObject.AddComponent<Rigidbody> ();
+		rbody.AddForce (Vector3.back);
+	}
+}
